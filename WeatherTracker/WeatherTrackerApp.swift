30069665e871a60f7051855e373065e44b340366
gravilaxe <joshuawright3132@outlook.com>
2023-02-15 17:23:21 +0000

    Initial Commit

---
+++ WeatherTracker/WeatherTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTrackerApp.swift
+//  WeatherTracker
+//
+//  Created by Joshua Wright on 15/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
